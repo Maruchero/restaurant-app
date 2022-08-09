3b0a0a055495796eff08bc5a7aeca413ee1f1c98
Under3nder <marce.gnz303@gmail.com>
2022-08-09 15:52:57 +0200

    Backend: URL parameters

--- 02- React + Django/backend/apis/urls.py
+++ 02- React + Django/backend/apis/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from .views import restaurantApi, menuApi, dishApi, areaApi, tableApi, orderApi, orderDishApi
+from .views import restaurant_api, menu_api, dish_api, area_api, table_api, order_api, order_dish_api
 
 urlpatterns = [
-    path('restaurant/', restaurantApi),
-    path('menu/', menuApi),
-    path('dish/', dishApi),
-    path('area/', areaApi),
-    path('table/', tableApi),
-    path('order/', orderApi),
-    path('orderDish/', orderDishApi),
+    path('restaurant/<str:method>/', restaurant_api),
+    path('menu/<str:method>/', menu_api),
+    path('dish/<str:method>/', dish_api),
+    path('area/<str:method>/', area_api),
+    path('table/<str:method>/', table_api),
+    path('order/<str:method>/', order_api),
+    path('orderDish/<str:method>/', order_dish_api),
 ]
--- 02- React + Django/backend/apis/views.py
+++ 02- React + Django/backend/apis/views.py
@@ -7,59 +7,104 @@ from .models import *
 from .serializers import *
 
 # Create your views here.
-def generalApi(request, base_class, base_serializer):
-    if (request.method == 'GET'):
-        objects = base_class.objects.all()
-        serializer = base_serializer(objects, many=True)
+def common_api(request, method, ObjectClass, ObjectSerializer):
+    print("\n### Common API")
+    print(method)
+    if method == "fetchAll":
+        objects = ObjectClass.objects.all()
+        serializer = ObjectSerializer(objects, many=True)
         return JsonResponse(serializer.data, safe=False)
 
-    elif (request.method == 'POST'):
+    elif method == "fetchById":
         data = JSONParser().parse(request)
-        serializer = base_serializer(data=data)
+        object_ = ObjectClass.objects.get(id=data['id'])
+        serializer = ObjectSerializer(object_)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif method == "add":
+        data = JSONParser().parse(request)
+        serializer = ObjectSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse("Added succesfully", safe=False)
-        return JsonResponse(serializer.errors, safe=False)        
+        return JsonResponse(serializer.errors, safe=False)
 
-    elif (request.method == 'PUT'):
+    elif method == "update":
         data = JSONParser().parse(request)
-        object_ = base_class.objects.get(id=data['id'])
-        serializer = base_serializer(object_, data=data)
+        object_ = ObjectClass.objects.get(id=data['id'])
+        serializer = ObjectSerializer(object_, data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse("Updated succesfully", safe=False)
         return JsonResponse(serializer.errors, safe=False)
 
-    elif (request.method == 'DELETE'):
+    elif method == "delete":
         data = JSONParser().parse(request)
-        object_ = base_class.objects.get(id=data['id'])
+        object_ = ObjectClass.objects.get(id=data['id'])
         object_.delete()
         return JsonResponse("Deleted succesfully", safe=False)
 
-@csrf_exempt
-def restaurantApi(request):
-    return generalApi(request, Restaurant, RestaurantSerializer)
 
 @csrf_exempt
-def menuApi(request):
-    return generalApi(request, Menu, MenuSerializer)
+def restaurant_api(request, method):
+    return common_api(request, method, Restaurant, RestaurantSerializer)
+
 
 @csrf_exempt
-def dishApi(request):
-    return generalApi(request, Dish, DishSerializer)
+def menu_api(request, method):
+    if (method == "fetchByRestaurantId"):
+        data = JSONParser().parse(request)
+        objects = Menu.objects.filter(restaurant=data['restaurantId'])
+        serializer = MenuSerializer(objects, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    return common_api(request, method, Menu, MenuSerializer)
+
 
 @csrf_exempt
-def areaApi(request):
-    return generalApi(request, Area, AreaSerializer)
+def dish_api(request, method):
+    if (method == "fetchByMenuId"):
+        data = JSONParser().parse(request)
+        objects = Dish.objects.filter(menu=data['menuId'])
+        serializer = DishSerializer(objects, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    return common_api(request, method, Dish, DishSerializer)
+
 
 @csrf_exempt
-def tableApi(request):
-    return generalApi(request, Table, TableSerializer)
+def area_api(request, method):
+    if (method == "fetchByRestaurantId"):
+        data = JSONParser().parse(request)
+        objects = Area.objects.filter(restaurant=data['restaurantId'])
+        serializer = AreaSerializer(objects, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    return common_api(request, method, Area, AreaSerializer)
+
 
 @csrf_exempt
-def orderApi(request):
-    return generalApi(request, Order, OrderSerializer)
+def table_api(request, method):
+    if method == "fetchByAreaId":
+        data = JSONParser().parse(request)
+        objects = Table.objects.filter(area=data['areaId'])
+        serializer = TableSerializer(objects, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    return common_api(request, method, Table, TableSerializer)
+
 
 @csrf_exempt
-def orderDishApi(request):
-    return generalApi(request, OrderDish, OrderDishSerializer)
+def order_api(request, method):
+    if method == "fetchByTableId":
+        data = JSONParser().parse(request)
+        objects = Order.objects.filter(table=data['tableId'])
+        serializer = OrderSerializer(objects, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    return common_api(request, method, Order, OrderSerializer)
+
+
+@csrf_exempt
+def order_dish_api(request, method):
+    if method == "fetchByOrderId":
+        data = JSONParser().parse(request)
+        objects = OrderDish.objects.filter(order=data['orderId'])
+        serializer = OrderDishSerializer(objects, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    return common_api(request, method, OrderDish, OrderDishSerializer)
