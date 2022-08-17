71eb9d7a5601232714252382eed71dbfc9fadbd1
Under3nder <marce.gnz303@gmail.com>
2022-08-17 18:07:59 +0200

    Added some data

--- 02- React + Django/backend/apis/admin.py
+++ 02- React + Django/backend/apis/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Restaurant, Menu, Dish, Area, Table, Order, OrderDish
+
+
+admin.site.register(Restaurant)
+admin.site.register(Menu)
+admin.site.register(Dish)
+admin.site.register(Area)
+admin.site.register(Table)
+admin.site.register(Order)
+admin.site.register(OrderDish)
